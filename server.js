ef7159fbfb28d2611f5ecb2c11abfda4810ddf08
rizal <rizalyogip@gmail.com>
2016-12-19 19:44:43 +0700

    ff

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var app = express();
-var router = require('./app/routes/api');
 app.set('view-engine','ejs');
 
-
-app.use(router);
 
 app.get('/cognitive',function(req,res){
     res.render('images.ejs');
